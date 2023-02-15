8aa2df26f2ecf05dc343e602cfb9a55639727db5
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2023-02-15 01:13:25 -0300

    new - tela login primeiros passos

--- crud-angular/src/app/app-routing.module.ts
+++ crud-angular/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { LoginComponent } from './login/login/login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {    path: '', pathMatch: 'full', redirectTo: 'login'  },
+  {
+    path: 'login',
+    loadChildren: () => import('./login/login.module').then(m => m.LoginModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- crud-angular/src/app/app.component.ts
+++ crud-angular/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+
 })
 export class AppComponent {
   title = 'crud-angular';
+hide: any;
+email: any;
+
 }
+export class FormFieldErrorExample {
+  email = new FormControl('', [Validators.required, Validators.email]);
+
+  getErrorMessage() {
+    if (this.email.hasError('required')) {
+      return 'You must enter a value';
+    }
+
+    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+}
+
+export class FormFieldPrefixSuffixExample {
+  hide = true;
+}
+
