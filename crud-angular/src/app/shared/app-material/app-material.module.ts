81c1a4c0317dc22a9205c269a702a5c9cef79f96
Fábio F S Jr <59260204+fabiofsjunior@users.noreply.github.com>
2023-02-26 18:19:41 -0300

    fix - ajustes de html css

--- crud-angular/src/app/shared/app-material/app-material.module.ts
+++ crud-angular/src/app/shared/app-material/app-material.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import { FormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -30,6 +32,7 @@ import { FormsModule } from '@angular/forms';
     MatProgressSpinnerModule,
     MatPaginatorModule,
     FormsModule,
+    MatSlideToggleModule,
 
   ],
 })
